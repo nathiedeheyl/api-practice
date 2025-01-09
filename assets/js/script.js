50dd0cf23c785d1e9a169725226bc4aca36fc339
Nathalie von <nath.vonheyl@gmail.com>
2025-01-09 11:36:10 +0000

    feat: fetch API key status and console log expiry date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-MzsvWWtCTRFpmBRj_fK3Ly_5M2g
+const API_KEY = "MzsvWWtCTRFpmBRj_fK3Ly_5M2g";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
