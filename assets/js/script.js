61f534af0fd89c4c66899521410d47d668623828
Nathalie von <nath.vonheyl@gmail.com>
2025-01-15 11:38:40 +0000

    refactor: copy ci source code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,18 @@ const API_KEY = "MzsvWWtCTRFpmBRj_fK3Ly_5M2g";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-document.addEventListener("DOMContentLoaded", () => {
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
-});
 
 function processOptions(form) {
     let optArray = [];
 
-    for (let entry of form.entries()) {
-        if (entry[0] === "options") {
-            optArray.push(entry[1]);
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
         }
     }
+
     form.delete("options");
 
     form.append("options", optArray.join());
@@ -23,6 +22,7 @@ function processOptions(form) {
 }
 
 async function postForm(e) {
+
     const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
@@ -41,25 +41,7 @@ async function postForm(e) {
         displayException(data);
         throw new Error(data.error);
     }
-}
 
-function displayErrors(data) {
-    let heading = `JSHint Results for ${data.file}`;
-
-    if (data.total_errors === 0) {
-        results = `<div class="no_errors">No errors reported!</div>`;
-    } else {
-        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
-        for (let error of data.error_list) {
-            results += `<div>At line <span class="line">${error.line}</span>, `;
-            results += `column <span class="column">${error.col}</span></div>`;
-            results += `<div class="error">${error.error}</div>`;
-        }
-    }
-
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
 }
 
 async function getStatus(e) {
@@ -76,6 +58,41 @@ async function getStatus(e) {
         displayException(data);
         throw new Error(data.error);
     }
+
+}
+
+function displayException(data) {
+
+    let heading = `<div class="error-heading">An Exception Occurred</div>`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
+
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayStatus(data) {
@@ -86,19 +103,6 @@ function displayStatus(data) {
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
-
     resultsModal.show();
-}
 
-function displayException(data) {
-    let heading = `An Exception Occured`;
-
-    results = `<div>The API returened status code ${data.status_code}</div>`;
-    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
-    results += `<div>Error text: <strong>${data.error}</strong></div>`;
-
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-
-    resultsModal.show();
 }
